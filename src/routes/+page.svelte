cb57f678e11c1ee102ae573eaeb640f6d9b2710f
Liande Guo <104030726+liandeguo@users.noreply.github.com>
2025-05-04 00:21:15 +0200

    added nav

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import { Menu } from '@lucide/svelte';
+</script>
+
+<nav class="flex w-full items-center justify-between bg-blue-400 p-3">
+	<span></span>
+	<img src="./logo.svg" class="h-10" alt="" style="filter: invert(1);" />
+	<Menu color="white" size="30" />
+</nav>
